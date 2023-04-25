32c015b0cbe21184786188a84f32019a86954c7e
Justin S <justinshyi123@gmail.com>
2023-04-25 03:40:07 -0700

    added more prints to doproduce dpc++ for testings

--- sql/core/src/main/scala/org/apache/spark/sql/execution/Columnar.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/Columnar.scala
@@ -231,7 +231,7 @@ case class ColumnarToRowExec(child: SparkPlan) extends ColumnarToRowTransition w
          |  arr[i] = (float) (columnartorow_mutableStateArray_2[0].getDouble(
          |    columnartorow_localEnd_0));
          |}
-         |
+         |System.out.println("before dpc++");
          |  //enter c++
          |boolean[] result = jnim.JNIOneapiCompareGreaterFloatArray(arr, (float) 20.0,
          |  columnartorow_localEnd_0 - 1, columnartorow_batchIdx_0);
