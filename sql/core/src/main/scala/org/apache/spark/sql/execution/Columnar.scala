f25902ae96c1fc4926c36333c2757818762e73b1
Justin S <justinshyi123@gmail.com>
2023-05-05 00:22:56 -0700

    test push

--- sql/core/src/main/scala/org/apache/spark/sql/execution/Columnar.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/Columnar.scala
@@ -220,7 +220,6 @@ case class ColumnarToRowExec(child: SparkPlan) extends ColumnarToRowTransition w
          |}
          |
          |
-         |
          |while ( columnartorow_mutableStateArray_1[0] != null) {
          |int columnartorow_numRows_0 = columnartorow_mutableStateArray_1[0].numRows();
          |int columnartorow_localEnd_0 = columnartorow_numRows_0 - columnartorow_batchIdx_0;
